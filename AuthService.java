b4e1768d74020ec489a1f1ae5f276f4628cdd1b0
Alex <alexkirilenko.r8@gmail.com>
2021-05-06 19:30:46 +0300

    NetWORK_2

---
+++ AuthService.java
@@ -0,0 +1,10 @@
+package NeTWORK_2;
+
+public interface AuthService {
+
+    String getNick(String login, String pass);
+
+    boolean login(String login, String pass);
+
+    boolean contains(String userName);
+}
